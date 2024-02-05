a6851d7d0fb3be30dabeb0096071be1edc04763f
sukhrobabdullaev <sukhrobjonabdullaev3365@gmail.com>
2024-02-05 12:15:58 +0500

    debugging

--- day05/leetcode.js
+++ day05/leetcode.js
@@ -4,3 +4,18 @@ function lengthOfLastWord(s) {
 }
 
 console.log(lengthOfLastWord("   fly me   to   the moon  "));
+
+function removeElement(nums, val) {
+  let j = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      // nums[1]=nums[1], nums[2]=nums[2]....
+      nums[j++] = nums[i];
+    }
+  }
+
+  return j;
+}
+
+console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2)); // 5
