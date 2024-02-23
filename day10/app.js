f004886cd6dd490666ab4dcabaf3ccd35a3a7da8
sukhrobabdullaev <sukhrobjonabdullaev3365@gmail.com>
2024-02-23 11:29:26 +0500

    sock merchant

--- day10/app.js
+++ day10/app.js
@@ -1,10 +1,20 @@
-let s = "the sky is blue";
+function sockMerchant(ar, n) {
+  const count = {};
 
-const newS = [];
-const originS = s.trim().split(" "); // 'the' 'sky' 'is' 'blue'
-console.log(originS.reverse().join(" "));
-for (let i = originS.length - 1; i > 0; i--) {
-  newS.push(s[i]);
+  for (let color of ar) {
+    count[color] = (count[color] || 0) + 1;
+  }
+  // console.log(count); // { '10': 4, '20': 3, '30': 1, '50': 1 }
+
+  let pairs = 0;
+
+  for (const color in count) {
+    const colorCount = count[color];
+    // console.log(colorCount); // 4 3 1 1
+    pairs += Math.floor(colorCount / 2);
+  }
+
+  return pairs;
 }
 
-console.log(newS.join(" "));
+console.log(sockMerchant([10, 20, 20, 10, 10, 30, 50, 10, 20], 9));
